c396a321a358531e4b786970d3efc1d1ede32ce5
lucatosc <toscano6210@gmail.com>
2024-11-11 19:09:39 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   const cartId = cookies().get('cartId')?.value;
   // Don't await the fetch, pass the Promise to the context provider
   const cart = getCart(cartId);
-
+   
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
