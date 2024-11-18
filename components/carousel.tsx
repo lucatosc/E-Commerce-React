60dd3b831835b4ed4a29d5d8c5aa3d141c04f811
lucatosc <toscano6210@gmail.com>
2024-11-18 03:07:29 -0800

    Update carousel.tsx

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -20,17 +20,7 @@ export async function Carousel() {
             className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
           >
             <Link href={`/product/${product.handle}`} className="relative h-full w-full">
-              <GridTileImage
-                alt={product.title}
-                label={{
-                  title: product.title,
-                  amount: product.priceRange.maxVariantPrice.amount,
-                  currencyCode: product.priceRange.maxVariantPrice.currencyCode
-                }}
-                src={product.featuredImage?.url}
-                fill
-                sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
-              />
+              
             </Link>
           </li>
         ))}
