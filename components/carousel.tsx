8aeeea7ebf45c6be9411e9bc4a7053fe78c3164f
lucatosc <toscano6210@gmail.com>
2024-11-18 03:08:16 -0800

    Update carousel.tsx

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -5,6 +5,7 @@ import { GridTileImage } from './grid/tile';
 export async function Carousel() {
   // Collections that start with `hidden-*` are hidden from the search page.
   const products = await getCollectionProducts({ collection: 'hidden-homepage-carousel' });
+  
   // Compare products length
   if (!products?.length) return null;
 
@@ -20,7 +21,17 @@ export async function Carousel() {
             className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
           >
             <Link href={`/product/${product.handle}`} className="relative h-full w-full">
-              
+              <GridTileImage
+                alt={product.title}
+                label={{
+                  title: product.title,
+                  amount: product.priceRange.maxVariantPrice.amount,
+                  currencyCode: product.priceRange.maxVariantPrice.currencyCode
+                }}
+                src={product.featuredImage?.url}
+                fill
+                sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
+              />
             </Link>
           </li>
         ))}
